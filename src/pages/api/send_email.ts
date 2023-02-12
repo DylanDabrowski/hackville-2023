09514bc3c9ba84e9386bf132dbbf46a8584f4db9
Dylan Dabrowski <dylandabrowski@gmail.com>
2023-02-11 20:15:52 -0500

    email sending working well, got indenting working as well

--- src/pages/api/send_email.ts
+++ src/pages/api/send_email.ts
@@ -4,43 +4,40 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-async function sendMail(
-  to: string,
-  from: string,
-  subject: string,
-  text: string
-) {
-  const msg = await {
-    to: to,
-    from: from,
+async function sendMail(to: string, subject: string, text: string) {
+  const recipients: string[] = to.split(" ");
+
+  // \n doesn't work in html tags, instead it needs to be a <br>
+  const htmlText = text.replace(/\n/g, "<br>");
+
+  const msg = {
+    to: recipients,
+    from: process.env.SENDGRID_SENDER_EMAIL,
     subject: subject,
     text: text,
-    html: "<strong>Hello World! My Name is Dylan :D</strong>",
+    html: `<p>${htmlText}</p>`,
   };
-  sgMail
+  await sgMail
     .send(msg)
     .then(() => {
       console.log("Email sent");
     })
     .catch((error: any) => {
+      console.error(error);
       return error;
     });
   return msg;
 }
 
-type Data = {
-  text: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse
 ) {
-  const { to, from, subject, text } = req.body;
-  try {
-    const response = await sendMail(to, from, subject, text);
-    res.status(200).json(response);
-  } catch (err) {
-    res.status(500).send({ error: "failed to fetch data" });
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
   }
+  res
+    .status(200)
+    .json(await sendMail(req.body.to, req.body.subject, req.body.text));
 }
